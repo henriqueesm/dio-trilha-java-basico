bd11aa2c47fc3ddcbb3de6b61d121941eb3939bf
henriqueesm <henriquefera.hsm@gmail.com>
2023-04-11 08:44:56 -0300

    Novo versionamento

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+
+    public static void main (String [] args) {
+
+        
+        int numero = 5;
+        
+        //numero = numero +1;
+
+        System.out.println(-- numero );
+    }
+}
